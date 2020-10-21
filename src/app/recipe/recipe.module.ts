6b4a0454eeda8a68ef0d2a85bd0c022fbf32649f
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 11:41:41 +0200

    Create recipe details component

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RouterModule } from '@angular/router';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 
 
 @NgModule({
-  declarations: [RecipeListComponent],
+  declarations: [RecipeListComponent, RecipeDetailsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
