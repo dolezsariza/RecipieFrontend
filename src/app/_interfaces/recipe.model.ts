b92b49a402998ce19a4192d403459bc63788736d
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 11:21:03 +0200

    Create model for recipe

---
+++ src/app/_interfaces/recipe.model.ts
@@ -0,0 +1,9 @@
+export interface Recipe{
+    ID: number; 
+    Name: string;
+    Description: string; 
+    OwnerName: string;
+    CategoryId: number; 
+    SubCategoryId: number;
+    Date: Date;
+}
