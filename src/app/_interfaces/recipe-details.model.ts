b8931220733386c971e8e578695b13e6c60f242e
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 14:22:51 +0200

    Add model for details

---
+++ src/app/_interfaces/recipe-details.model.ts
@@ -0,0 +1,15 @@
+export interface RecipeDetails{
+    Id: number; 
+    Name: string;
+    Description: string; 
+    OwnerName: string;
+    Energy: number;
+    Fat: number;
+    Carbohydrate: number;
+    Sugar: number;
+    Protein: number;
+    Salt: number;
+    Date: Date;
+    // Tags: string[];
+    // Ingredients: string[];
+}
