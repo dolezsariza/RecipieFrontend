50be6c2b63a17ae9b18326795a382f6605a8718b
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 14:24:01 +0200

    Remove description and date from recipe model

--- src/app/_interfaces/recipe.model.ts
+++ src/app/_interfaces/recipe.model.ts
@@ -1,9 +1,7 @@
 export interface Recipe{
     ID: number; 
     Name: string;
-    Description: string; 
     OwnerName: string;
     CategoryId: number; 
     SubCategoryId: number;
-    Date: Date;
 }
