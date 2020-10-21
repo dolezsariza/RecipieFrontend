28b2d82834c5898392d6caf3b73043a297cf3d32
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 11:40:50 +0200

    Add routing for error 500

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
-import { NotfoundComponent } from './notfound/notfound.component';
+import { NotfoundComponent } from './error-pages/notfound/notfound.component';
 import { HttpClientModule } from '@angular/common/http';
+import { InternalServerComponent } from './error-pages/internal-server/internal-server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    InternalServerComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '404', component : NotfoundComponent},
+      { path: '500', component: InternalServerComponent },
       { path: 'recipe', loadChildren: () => import('./recipe/recipe.module').then(m => m.RecipeModule) },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: '**', redirectTo: '/404', pathMatch: 'full'}
