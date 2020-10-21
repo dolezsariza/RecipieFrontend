8c8e35f99ce1454d4ad4c201289eaef280109efe
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-21 14:24:12 +0200

    Create method to get details

--- src/app/recipe/recipe-details/recipe-details.component.ts
+++ src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,4 +1,9 @@
+import { RecipeDetails } from './../../_interfaces/recipe-details.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ErrorHandlerService } from 'src/app/shared/services/error-handler.service';
+import { RepositoryService } from 'src/app/shared/services/repository.service';
+import { Recipe } from 'src/app/_interfaces/recipe.model';
 
 @Component({
   selector: 'app-recipe-details',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailsComponent implements OnInit {
 
-  constructor() { }
+  public recipeDetails: RecipeDetails;
+  public errorMessage: string = "";
+  public finished = false;
+
+  constructor(private repository: RepositoryService, private router: Router, 
+              private activeRoute: ActivatedRoute, private errorHandler: ErrorHandlerService) { }
 
   ngOnInit(): void {
+    this.getRecipeDetails();
+  }
+
+  getRecipeDetails = () => {
+    let id: number = this.activeRoute.snapshot.params["id"];
+    let apiUrl: string = `recipes/${id}`;
+
+    this.repository.getData(apiUrl)
+    .subscribe(res => {
+      this.recipeDetails = res as RecipeDetails;
+      this.finished = true;
+    },
+    (error) =>{
+      this.errorHandler.handleError(error);
+      this.errorMessage = this.errorHandler.errorMessage;
+    })
   }
 
 }
