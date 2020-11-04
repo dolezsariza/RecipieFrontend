595e76f60f37dff1bdc6c8614fb53fa2f7d11e0f
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-11-04 12:23:06 +0100

    Add functionality to login component

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ErrorHandlerService } from 'src/app/shared/services/error-handler.service';
+import { RepositoryService } from 'src/app/shared/services/repository.service';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  user: any = {}
+  public errorMessage: string = "";
+  constructor(private fb: FormBuilder, private repository: RepositoryService, private errorHandler: ErrorHandlerService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm(){
+    this.loginForm = this.fb.group({
+      userName: [null, Validators.required],
+      password:[null, Validators.required]
+    });
+  }
+
+  loginUser = () => {
+    let apiAddress: string = "login";
+    this.repository.create(apiAddress, this.user)
+    .subscribe(result => {
+      console.log(result);
+    },
+    (error) => {
+      this.errorHandler.handleError(error);
+      this.errorMessage = this.errorHandler.errorMessage;
+    })
+  }
+
+  //-------------------------------------
+  // Getter methods for all form controls
+  //-------------------------------------
+  get userName(){
+    return this.loginForm.get('userName') as FormControl;
+  }
+  get password(){
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  onSubmit(){
+    this.user = Object.assign(this.user, this.loginForm.value);
+    this.loginUser();
+    console.log(this.user);
   }
 
 }
