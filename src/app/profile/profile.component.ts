79475c823ace35d69f41b46ec184c25b476df6bf
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-11-05 15:13:46 +0100

    Add edit option to profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ErrorHandlerService } from 'src/app/shared/services/error-handler.service';
 import { RepositoryService } from '../shared/services/repository.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,9 +13,12 @@ import { RepositoryService } from '../shared/services/repository.service';
 export class ProfileComponent implements OnInit {
 
   public user: any;
+  profileData: any = {};
+  underEditing: boolean = false;
   errorMessage: string = "";
-
-  constructor(private repository: RepositoryService, private errorHandler: ErrorHandlerService) { }
+  editForm: FormGroup;
+  editSuccessful: boolean = true;
+  constructor(private fb: FormBuilder, private repository: RepositoryService, private errorHandler: ErrorHandlerService,private router: Router) { }
 
   ngOnInit(): void {
     this.getProfile();
@@ -29,6 +34,51 @@ export class ProfileComponent implements OnInit {
       this.errorHandler.handleError(error);
       this.errorMessage = this.errorHandler.errorMessage;
     })
+  }
+
+  createEditForm(){
+    this.editForm = this.fb.group({
+      firstName: [this.user.firstName],
+      lastName: [this.user.lastName],
+      introduction: [this.user.introduction]
+    });
+  }
+
+  editProfile(){
+    this.underEditing = true;
+    this.createEditForm();
+  }
+
+  saveProfile(){
+    let apiAddress: string = `profile/${localStorage.getItem('user')}`;
+    this.repository.update(apiAddress,this.profileData)
+    .subscribe(result => {
+      console.log(result);
+      this.errorMessage = "";
+      this.editSuccessful = true;
+    },
+    (error) => {
+      this.errorHandler.handleError(error);
+      this.errorMessage = this.errorHandler.errorMessage;
+      console.log(this.errorMessage);
+      this.editSuccessful = false;
+    })
+  }
+
+  cancel(){
+    this.underEditing = false;
+  }
+
+  onSubmit(){
+    this.profileData = Object.assign(this.profileData, this.editForm.value);
+    console.log(localStorage.getItem('user'));
+    this.saveProfile();
+    setTimeout(() => {
+      if(this.errorMessage === ""){
+        this.router.navigate(['/profile'])
+      }
+      console.log(this.errorMessage);
+    }, 2000);
   }
 
 }
